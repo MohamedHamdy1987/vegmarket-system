4d39d7a69dfd8fd1407362fc5b2e9349dbf786c2
MohamedHamdy1987 <shahat10101048@gmail.com>
2026-04-06 18:50:43 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
+const SUPABASE_URL = 'https://lfhrorjiukzkqhafjtdd.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmaHJvcmppdWt6a3FoYWZqdGRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3OTc3NTgsImV4cCI6MjA5MDM3Mzc1OH0.eQ0w4DG_-DNvnJRJxgvJ7KhNNkBhOEswQhtbiO2my3Q';
+
+export const sb = createClient(SUPABASE_URL, SUPABASE_KEY);
